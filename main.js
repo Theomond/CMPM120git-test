a1fae4c195a79cc3d5d6416ee80e88ada8310e51
Muhammad <eaglerangers.galbatrox@gmail.com>
2018-05-11 12:11:14 -0700

    added more ints

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ console.log("how do you do?");
 console.log("What is happening?");
 console.log("Mwa ha ha Lets create a merge conflict");
 
-int x = 1;
+int x = 2;
 int y = 2;
+int z = 3;
+int a = 5;
